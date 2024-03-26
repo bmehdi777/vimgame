0c86abd9a8fb3d8dd9308fc810a58e40a2898361
qquentinv <50825643+qquentinv@users.noreply.github.com>
2024-03-26 15:34:38 +0100

    chore: error in push

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Action {
+	level: number;
   title: string;
   input: string[];
 }
 enum Result {
   WIN = "Nice!",
   LOOSE = "Nope...",
+	NEXT_LEVEL = "Yeah, new level unlock!"
 }
 
 interface Input {
@@ -15,21 +17,43 @@ interface Input {
 }
 
 const listAction: Action[] = [
-  { title: "Save", input: [":w"] },
-  { title: "Save and close", input: [":wq", ":x"] },
-  { title: "Save all", input: [":wa"] },
-  { title: "Save and close all", input: [":wqa", ":xa"] },
+  { level: 1, title: "Save", input: [":w"] },
+  { level: 1, title: "Save and close", input: [":wq", ":x"] },
+  { level: 1, title: "Save all", input: [":wa"] },
+  { level: 1, title: "Save and close all", input: [":wqa", ":xa"] },
+	{ level: 2, title: "Append", input: ["a"] },
+	{ level: 2, title: "Insert", input: ["i"] },
+	{ level: 2, title: "End of line", input: ["$"] },
+	{ level: 2, title: "Up", input: ["k"] },
+	{ level: 3, title: "Down", input: ["j"] },
+	{ level: 3, title: "Left", input: ["h"] },
+	{ level: 3, title: "Right", input: ["l"] },
+	{ level: 4, title: "End of word", input: ["e"] },
+	{ level: 5, title: "Start of word", input: ["b"] }, 
+	{ level: 6, title: "Append end of line", input: ["$a", "A"] },
 ];
 
-const generateRandomAction = () => {
-  const numActions = listAction.length;
+const listLevel: Level[] = [
+	{ name: "Novice", points: 4 },
+	{ name: "Apprentice", points: 9 },
+	{ name: "Journeyman", points: 16 },
+	{ name: "Expert", points: 31 },
+	{ name: "Master", points: 54 },
+	{ name: "Virtuoso", points: 91 } // This number has history, please never change
+];
+
+const generateRandomAction = (level: number) => {
+	const listByLevel = listAction.filter(action => action.level === 2);
+  const numActions = listByLevel.length;
   const selected = Math.floor(Math.random() * numActions);
   return listAction[selected];
 };
 
+
 function App() {
+	const [level, setLevel] = useState<number>(1);
   const [currentAction, setCurrentAction] = useState<Action>(
-    generateRandomAction(),
+    generateRandomAction(level),
   );
   const [result, setResult] = useState<Result>();
   const [points, setPoints] = useState<number>(0);
@@ -38,23 +62,30 @@ function App() {
 
   const onSubmit: SubmitHandler<Input> = (data) => {
     if (currentAction.input.includes(data.userInput)) {
-      setResult(Result.WIN);
+			if (points !== 0 && points % 4 === 0) {
+				setResult(Result.NEXT_LEVEL);
+				setLevel(level + 1);
+   	  } else {
+        setResult(Result.WIN);
+			} 
       setTimeout(() => {
-        const random = generateRandomAction();
-        setCurrentAction(random);
-        setResult(undefined);
-        resetField("userInput");
-        setPoints(points + 1);
-      }, 1000);
+          const random = generateRandomAction();
+          setCurrentAction(random);
+          setResult(undefined);
+          resetField("userInput");
+          setPoints(points + 1);
+       }, 1000);
     } else {
       setResult(Result.LOOSE);
 			setPoints(0);
+			setLevel(1);
     }
   };
 
   return (
     <>
-      <p>Streak : {points}</p>
+			<p>Level: {listLevel[level - 1].name}</p>
+      <p>Streak: {points}</p>
       {currentAction.title}
       <form onSubmit={handleSubmit(onSubmit)}>
         <input type="text" {...register("userInput")} autoFocus/>
